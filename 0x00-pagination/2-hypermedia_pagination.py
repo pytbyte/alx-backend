8a279f7783d1b428ab91e6bb014f197f9269bc54
pytbyte <heretolearn1@gmail.com>
2024-02-09 09:46:42 +0300

    pagnation late upload

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -50,18 +50,18 @@ class Server:
 
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-            """Get more information about a page.
-            """
-            assert isinstance(page, int) and page > 0
-            assert isinstance(page_size, int) and page_size > 0
+        """Get more information about a page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-            page_data = self.get_page(page, page_size)
-            total_pages = math.ceil(len(self.dataset()) / page_size)
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
-            next_page = page + 1 if page < total_pages else None
-            prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
 
-            return {
+        return {
                 "page_size": len(page_data),
                 "page": page,
                 "data": page_data,
@@ -69,4 +69,3 @@ class Server:
                 "prev_page": prev_page,
                 "total_pages": total_pages
             }
-    
